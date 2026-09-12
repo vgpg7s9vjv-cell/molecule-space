54d415a67ea3ebcb70130c0dcd114071e65071f0
vgpg7s9vjv-cell <vgpg7s9vjv@privaterelay.appleid.com>
2026-09-13 00:55:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -251,12 +251,12 @@ links: {
       content: `
         <div class="official-links">
 
-          <a
+          <a>
             class="official-link-card"
             href="https://t.me/+8m49Mte-Um5lMGUy"
             target="_blank"
             rel="noopener noreferrer"
-          >
+         
             <div class="official-link-icon">✦</div>
 
             <div class="official-link-info">
@@ -268,12 +268,12 @@ links: {
           </a>
 
 
-          <a
+          <a>
             class="official-link-card"
             href="https://t.me/+77xMDKXa0Zk3MTIy"
             target="_blank"
             rel="noopener noreferrer"
-          >
+          
             <div class="official-link-icon">☁</div>
 
             <div class="official-link-info">
@@ -285,12 +285,12 @@ links: {
           </a>
 
 
-          <a
+          <a>
             class="official-link-card"
             href="https://t.me/otzovisarttmolecule"
             target="_blank"
             rel="noopener noreferrer"
-          >
+          
             <div class="official-link-icon">♡</div>
 
             <div class="official-link-info">
@@ -302,12 +302,12 @@ links: {
           </a>
 
 
-          <a
+          <a>
             class="official-link-card"
             href="https://t.me/anonaskbot?start=molecule"
             target="_blank"
             rel="noopener noreferrer"
-          >
+          
             <div class="official-link-icon">?</div>
 
             <div class="official-link-info">
@@ -319,12 +319,12 @@ links: {
           </a>
 
 
-          <a
+          <a>
             class="official-link-card"
             href="https://www.tiktok.com/@arttmolecule1?_r=1"
             target="_blank"
             rel="noopener noreferrer"
-          >
+          
             <div class="official-link-icon">♪</div>
 
             <div class="official-link-info">
