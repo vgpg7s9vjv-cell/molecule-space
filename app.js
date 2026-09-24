5655e7a934aec67673632d9858a9d85e1db87a17
vgpg7s9vjv-cell <vgpg7s9vjv@privaterelay.appleid.com>
2026-09-24 11:17:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2626,7 +2626,7 @@ function calculateEnergy() {
   const result = document.getElementById("energyResult");
   if (!age || age < 13 || !weight || !height) { result.textContent = "УПС! твой организм еще растет и тебе не нужен дефицит"; return; }
   const bmr = 10 * weight + 6.25 * height - 5 * age - 161;
-  if (!sex || sex <= "male") { bmr = 0 * weight + 6.25 * height - 5 * age + 5 }
+  if (!sex || sex = "male") { const bmr = 10 * weight + 6.25 * height - 5 * age + 5; }
   const tdee = bmr * activity * 0.8;
   result.innerHTML = `основное количество сжигаемых тобой калорий с учетом активности ${Math.round(bmr)} ккал/сутки<br><strong>твой идельный дефицит калорий (20% от суточной нормы): ${Math.round(tdee)} ккал/сутки</strong>`;
 }
